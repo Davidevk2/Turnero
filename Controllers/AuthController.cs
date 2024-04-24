3058f1dae4a78ae11ad01ff0373e4cae85eb48d3
Davidhzk2 <guffyherrera150@gmail.com>
2024-04-24 12:22:20 -0500

    Controlador y vista de login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Turnos.Controllers
+{
+    
+    public class AuthController : Controller
+    {
+       
+       public readonly TurnosContext _context;
+
+       public AuthController(TurnosContext context){
+        _context = context;
+       }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(string correo, string password){
+
+            return Json(correo+password);
+            // var usuarioLoggeado = await  _context.Recepcions.FirstOrDefaultAsync(em => em.correo == identificacion);
+
+            // if(usuarioLoggeado != null && usuarioLoggeado.Password == password){
+            //     HttpContext.Session.SetString("Nombre", usuarioLoggeado.Nombres); //crear variable de sesion 
+            //     return RedirectToAction("Index", "Empleados");
+            // }else{
+
+            //  return RedirectToAction("Index"); //retornar al login
+            // }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Logout(){
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Recepcion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turnos.Models
+{
+    public class Recepcion
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string  password { get; set; }
+    }
+}
